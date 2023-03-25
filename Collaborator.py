9744670e69c0f520aad303e49d814eeb38146d83
Alex Flynn <aflynn0213@gmail.com>
2023-03-25 14:02:04 -0500

    trying to add engine class

--- Collaborator.py
+++ Collaborator.py
@@ -9,14 +9,33 @@ import numpy as np
 from sklearn.metrics.pairwise import cosine_similarity
 import pandas as pd
 
-class Collaborator:
+def calc_similarity(df,opt):
+    norm = adjusted_cos(df,opt) 
+    labels,norm = df.index,norm if opt == 1 else df.columns,norm.T
+    return pd.DataFrame(data=cosine_similarity(norm),columns=labels,index=labels)
     
-    def __init__(self,df):
+def adjusted_cos(df,opt):
+    if(opt==1):
+        return df.sub(df.mean(axis=1,skipna=True),axis=0)
+    elif(opt==2):
+        return df.sub(df.mean(axis=0,skipna=True),axis=1)
+
+'''class Collaborator:
+    
+    def __init__(self,df,opt):
         self.df = df
-        self.sim_ = calc_similarity()
+        self.norm = self.adjusted_cos(opt)
+        self.sim = self.calc_similarity(opt)
         
-    def calc_similarity(self):
-        return self.df.T.corr()
+    def calc_similarity(self,opt):
+        labels = self.df.index if opt == 1 else self.df.columns
+        return pd.DataFrame(data=cosine_similarity(self.norm.T),columns=labels,index=labels)
         
-    #def adjusted_cos(self):
-     #   mean = self.df.mean(axis=1)
+    def adjusted_cos(self,opt):
+        if(opt==1):
+            return self.df.sub(self.df.mean(axis=1,skipna=True),axis=0)
+        elif(opt==2):
+            return self.df.sub(self.df.mean(axis=0,skipna=True),axis=1)
+       
+'''
+   
--- main.py
+++ main.py
@@ -1,109 +1,15 @@
 #!/usr/bin/python3
-import pandas as pd
-import numpy as np
-import time
-import matplotlib.pyplot as plt
+import argparse
 
-#USER CREATED LIBRARIES
-from DataProcessor import DataProcessor
-#from MatrixFactorizer import MatrixFactorizer
-import MatrixFactorizer as mf
+from engine import Engine
 
-def topTenPresentation(dp,recoMat,userId):
-    temp_row = recoMat.loc[userId]
-    top10 = temp_row.nlargest(10)
-    print("RESULTS.......")
-    count = 1
-    for film in top10.index:
-        title = dp.fetch_title(dp.moviedId_tmdbId_map(film))
-        time.sleep(2.5)
-        print(str(count)+") "+title)
-        count+=1
-
+   
 if __name__ == '__main__':
-    valid_val = True
-
-    while(valid_val):
-        print("WOULD THE USER LIKE THE FULL DATABASE OR THE LITE VERSION: ")
-        opt = int(input("1 - FULL\n2 - LITE (RECOMMENDED)\n"))
-        if (opt != 1 and opt != 2):
-            print("INVALID VALUE! TRY AGAIN")
-            continue
-        else:
-            dp = DataProcessor(opt)
-            valid_val = False
-
-    print("STEP 1 Just Read in CSVs")
-
-
-    print("STEP 2 Pivotting User Ratings csv to create SIM MATRIX")
-    sim_mat = dp.rates.pivot(index='userId',columns='movieId',values='rating')
-    sim_mat.fillna(0,inplace=True)
-    test = Cosine_Collaborator(sim_mat)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('algorithm',type=int,default=1,help="Algorithm to run with options:{1)gradient, 2)cosine}")
+    args = parser.parse_args()
     
-    print("STEP 3 ABOUT TO RUN GRADIENT DESCENT ON USER AND MOVIE MATRICES")
-    U,M,err = mf.gradient_handler(sim_mat,20)
+    eng = Engine(args.algorithm)
     
-    print("STEP 4 DOTTING USER AND MOVIE MATRICES TO FORM RECOMMENDATION MATRIX")
-    reco_mat = np.dot(U,M)
-
-    print("STEP 5 Writing to csv files")
-    #TURN INTO PANDAS DATAFRAME
-    reco_mat = pd.DataFrame(reco_mat,index=sim_mat.index,columns=sim_mat.columns)
-    #sim_mat.to_csv('sim_mat.csv',encoding='utf-8')
-    #reco_mat.to_csv('recommendation_matrix.csv',encoding ='utf-8')
-    #orig_mat = np.where(gd_mat != 0, 1, 0)
-
-    user_opt = 0
-    valid_opts = [1,2,3,4]
-    while(user_opt != 3):
-        print("\nWOULD YOU LIKE TO SEE A MOVIE RECOMMENDED FOR A CERTAIN USER OR YOURSELF?")
-        user_opt = int(input("1)USER ID\n2)YOURSELF\n3)QUIT\n4)METRICS\n"))
-        if (user_opt not in valid_opts):
-            print("TRY AGAIN, VALID OPTIONS ARE 1-4\n")
-            continue 
-        
-        elif (user_opt == 1):
-            userID_input = int(input("USER ID: "))
-            if (userID_input not in reco_mat.index):
-                print("INVALID USER ID, EXITING......")
-            else:
-                topTenPresentation(dp, reco_mat, userID_input)
-        
-        elif (user_opt == 2):
-            print("WE ARE GOING TO GENERATE A RANDOM LIST OF MOVIES\n")
-            print("YOU HAVE THE OPTION OF RANKING THE MOVIE FROM 1-5 STARS\n")
-            print("YOU CAN DO THIS INCREMENTS OF .5 STARS\n")
-            print("IF YOU HAVEN'T SEE THE FILM THERE WILL BE A SKIP OPTION\n")
-            print("PLEASE SKIP IF YOU HAVEN'T SEEN THE FILM!\n")
-            print("PRESENTING FILMS NOW......")
-            newRates,userId = dp.rand_movie_rater()
-            sim_mat = sim_mat.append(newRates,ignore_index=True,sort=False)
-            sim_mat.fillna(0,inplace=True)
-            U,M,error = mf.gradient_handler(sim_mat,25)
-            reco_mat = np.dot(U,M)
-            reco_mat = pd.DataFrame(reco_mat,index=sim_mat.index,columns=sim_mat.columns)
-            topTenPresentation(dp, reco_mat, userId)
-        
-        elif (user_opt == 3):
-            print("THANK YOU FOR USING THE MOVIE RECOMMENDER\n")
-            print("HOPE TO SEE YOU SOON!\n")
-        
-        elif (user_opt==4):
-            feat_arr = [2,5,10,20,30,40,50]
-            feat_dict = dict()
-            for i in feat_arr:
-                print("Running Gradient Descent with " + str(i) + " features")
-                U,M,err = mf.gradient_handler(sim_mat, i)
-                feat_dict[i] = err
-            ky = min(feat_dict)
-            min_ky = min(feat_dict, key = lambda k:feat_dict[k])
-            fig = plt.figure(figsize = (10, 5))
-            plt.plot(feat_dict.keys(),feat_dict.values())
-            plt.xlabel("# Features")
-            plt.ylabel("Error")
-            plt.title("Plot of Error vs Features")
-            plt.show()
-            
-            print("Number of Features w/ Minimum Error: ", min_ky)
-            print("Error by # of features", feat_dict)
+    eng.run()
+    
