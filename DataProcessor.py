db621261daa2da0e22d1831ad756975c97276741
aflyn <aflynn0213@gmail.com>
2023-05-02 11:27:28 -0500

    Comments everywhere and added 0.5 rating as valid

--- DataProcessor.py
+++ DataProcessor.py
@@ -19,45 +19,76 @@ class DataProcessor:
         with Pool(processes=3) as p:
             self.movies,self.rates,self.links = p.map(self.read_data,files)
         
+        #links is the mapping between the movie ids in our rating matrix
+        #and the tmdbIds that appear in the movies_metadata csv file
+        #and allow us to pull titles from to give user meaningful info
         self.links.set_index("movieId",inplace=True)
+        #our movies dataframe only contains id (which is the tmdbId), title,
+        #and original language. Setting ID to index
         self.movies.set_index("id",inplace=True)
+        #Grab all unique movieIds from the ratings matrix 
         self.uniq_movs = self.rates.movieId.unique()
+        #Find unique tmdbId corresponding to all movieIds that
+        #actually have ratings and are useful for our data
         tmdb_ids = self.links.tmdbId.unique()
+        #want the compliment (~) of NaN values since we need 
+        #a valid tmdbId in order to fetch a title
         tmdb_ids = tmdb_ids[~np.isnan(tmdb_ids)]
+        #want all tmdbIds as appear in movies metadata file 
+        #in order to filter out movies whose tmdbIds don't show up
+        #in this file
         metadata_ids = self.movies.index.unique()
+
         filtered_movies = []
         self.links['tmdbId']=self.links['tmdbId'].fillna(0)
-        
+        print("STEP Filtering out invalid tmdbIds")
+        #filter out movies who don't have a tmdbId in the links dataframe
         for movie in self.uniq_movs:
             if(self.links.at[movie,"tmdbId"]==0):
                 filtered_movies.append(movie)
+                #DEBUG
                 print(movie)
-                
+        #second filtering for movies whose tmdbIds don't appear in movies metadata        
         for tmdb in tmdb_ids:
+            #tmdb_ids is a numpy array so we want to cut the .0 at the end by converting to int then string
             if str(int(tmdb)) not in metadata_ids:
+                #the index of links is the movieId which are the ones in our user-movie rating matrix, so
+                #for invalid tmdbIds we want to grab the correct "movieId" in order to filter out the 
+                #movie from our ratings matrix
                 Id = self.links.index[self.links.tmdbId == tmdb].tolist()
+                #since list is only one element we can just grab first element
                 Id = Id[0]
                 filtered_movies.append(Id)
+                #DEBUG
                 print(Id)
                 
-            
+        #Gets rid of rows where the movieId is in the filtered_movies list 
+        #permanently removing movies that can't map to a title
         self.rates = self.rates[self.rates.movieId.isin(filtered_movies)==False]
+        #Used in order to grab a random title in the main file to allow user to 
+        #rate movies and establish a preference profile
         self.uniq_movs = self.rates.movieId.unique()
+        #Timestamp is meaningless
         self.rates.drop('timestamp',axis=1, inplace=True)
+        #ratings used as official user-movie rating matrix throughout
         self.ratings = self.rates
+        #pivot and used to write to a csv for insights
         self.rates = self.rates.pivot(index='userId',columns='movieId',values='rating')
         self.rates.to_csv('filtered_rates.csv')
-               
+    
+    #maps movieId from rating matrix to tmdbId used to find a title from movies metadata
     def moviedId_tmdbId_map(self,mov_id):
-        temp = self.links
-        tmdb = temp.at[mov_id,"tmdbId"]
+        tmdb = self.links.at[mov_id,"tmdbId"]
         return str(int(tmdb))
-        
+     
+    #_id must be a valid tmdbId in order to fetch a title from movies metadata 
     def fetch_title(self,_id):
         valid = _id in self.movies.index
         return (self.movies.at[_id,"title"] if valid else "NOT IN DATABASE")
-     
+    
+    #used to read in csvs and create pandas dataframes
     def read_data(self,x):
+        #filters out everything aside from the mentioned columns
         if 'movies_metadata.csv' in x.lower():
             return (pd.read_csv(x,usecols=["id","original_language","title"],dtype={"id":"string","title":"string"})) #[lambda m: m["original_language"]=="en"])
         else:
--- engine.py
+++ engine.py
@@ -22,17 +22,31 @@ class Engine:
         print("STEP Reading in Data from CSVs")
         self.dp = DataProcessor()
         print("STEP Creating Training, Test, and AntiTest sets")
-        self.reader = Reader(rating_scale=(1,5)) 
+        self.reader = Reader(rating_scale=(0.5,5)) 
+        #Full data set reads in our user movie ratings 
         self.data = Dataset.load_from_df(self.dp.ratings,self.reader)
+        #Instance of trainset class from surprise
         self.fullTrain = self.data.build_full_trainset()
+        #Test set is a list of ratings that can be used in test method later
+        #returns only the ratings that have an actual rating
         self.test_set = self.fullTrain.build_testset()
+        #returns all the ratings not in the trainset (all the ratings that are unknown)
+        #used in predictions as this will predict all the unknown ratings and therefore
+        #when we sort by highest predictions in this set, it will already be narrowed down
+        #the movies unseen by a given user and will not require us to do further filtering to
+        #only generate unseen movies i.e. it handles the filtering for us since 
+        #this test set is only unobserved user-movie interactions to begin with
         self.antiTest = self.fullTrain.build_anti_testset(fill=0)
+        #METRICS option to provide a table of simple statistics on a handful of algorithms
         if algorithm == "METRICS":
             self.performance_df = self.performance_metrics()
+        #All options aside from metrics (a specific algorithm was chosen from index.html)
         else:
             self.best_est = self.hyperparameter_tuning()
             self.preds = self.best_est.fit(self.fullTrain).test(self.antiTest)
-        
+     
+    #takes the given algorithm and runs gridsearch cv in order to find the best estimator 
+    #and then returns this best estimator
     def hyperparameter_tuning(self):
         print("STEP Picking Algorithm: ")
         print(self.algorithm)
@@ -67,6 +81,10 @@ class Engine:
         
         return cv_obj.best_estimator["rmse"]     
     
+    #Used as the function in the multiprocessing pool when running METRICS
+    #Performs gridsearch on large number of parameter combos and returns FCP, RMSE
+    #and Best Parameters for each algorithm
+    #Tests on whole dataset in order to best avoid overfitting
     def test_bestCV_alg(self,algo,params):
         gs_cv = GridSearchCV(algo,params,measures=['rmse'],cv=3,n_jobs=-1,joblib_verbose=2)
         gs_cv.fit(self.data)
@@ -81,7 +99,10 @@ class Engine:
         rmse_score = accuracy.rmse(preds,verbose=True)
 
         return [fcp_score,rmse_score,b_params]
-        
+    
+    #Creates parameter combos used in test_bestCV_alg and then 
+    #returns dataframe with all the algorithms tested and their
+    #statistics used to display in the metrics.html webpage
     def performance_metrics(self):
         print("STEP In performance metrics")
         u_mean = self.fullTrain.global_mean
@@ -98,15 +119,6 @@ class Engine:
                     'reg_all': [0.02, 0.05], 
                     'verbose': [True]}
         params.append(params1)
-
-
-        # params2 = { 'n_factors': [20, 40],
-        #             'n_epochs': [20, 30], 
-        #             'lr_all': [0.005, 0.07],
-        #             'reg_all': [0.02, 0.05],
-        #             'verbose': [True],
-        #             'cache_ratings': [True]}
-        # params.append(params2)
 
         params3 = {'bsl_options': { 'method': ['als'],
                                     'n_epochs': [5, 10, 15],
@@ -127,29 +139,37 @@ class Engine:
                      'sim_options': {'name': ['cosine', 'pearson_baseline'], 'user_based': [True], 'shrinkage':[25,50,100], 'min_support': [3]},
                     'verbose': [True]}
         params.append(paramsKnn)
-        # params.append(paramsKnn)
-        # params.append(paramsKnn)
         
         print("STEP About to start  multi-threaded gridsearch CV and test")
+        #Creates tuples of (algorithm,parameter grid) combo since each
+        #algorithm has one associated parameter grid with it
+        #i.e. cv_args = [(SVD,params1),(BaselineOnly,params3),etc....
         cv_args = list(zip(algs,params))
+        #instance of multiprocessing object
         pool = Pool()
         partial_cv = partial(self.test_bestCV_alg)
+        #starmap---> [(1,2), (3, 4)] results in [partial(1,2), partial(3,4)]
         scores = pool.starmap(partial_cv,cv_args)
-
+        
+        #End pool processes
         pool.close()
         pool.join()
+        print("STEP finished multiprocressing")
 
-
+        #Rename since prior name was just BaselineOnly, BaselineOnly
         algs[1] = "ALS"
         algs[2] = "SGD"
 
         algs = [str(x) for x in algs]
         print("STEP Packing results from CV and testing into Dataframe")
+        #scores from above is a list of list where each sublist was composed of
+        #[FCP, RMSE, cv.best_params]
         df_data = { 'Algorithm': algs,
                     'FCP': [score[0] for score in scores],
                     'RMSE': [score[1] for score in scores],
                     'Best Parameter Set': [score[2] for score in scores]}
-
+        
+        #Create dataframe used for html presentation
         df = pd.DataFrame(df_data)
         df.set_index('Algorithm',inplace=True)
 
@@ -157,9 +177,22 @@ class Engine:
         
         
     def run_new_user(self,df):
+        #concat at axis=0 adds new user vector as row in ratings 
         self.dp.ratings = pd.concat([self.dp.ratings,df],axis=0)
+        #creates new dataset due to update to ratings with new user addition
         self.data = Dataset.load_from_df(self.dp.ratings,self.reader)
+        #creates new trainset with new user entry
         self.fullTrain = self.data.build_full_trainset()
+        #creates new antitestset to predict unseen movies
         self.antiTest = self.fullTrain.build_anti_testset(fill=0)
+        #Uses best_estimator from EARLIER when the engine object was first initialized and 
+        #tested against the entire data set in order to see how our previously cross valdiated
+        #model generalized to new data.  It should be the best performing and most adaptive
+        #to new data as this best estimator parameter combination achieved the highest score in 
+        #the prior grid search cross validation step after the User chose an algorithm to initialize with
+        #and because it was previously tested against the ENTIRE dataset and cross validated,
+        #ideally it shouldn't be prone to overfitting that data and should generalize well.
+        #Furthermore, we test it on the antitestset to limit our scope of predictions to only unseen
+        #movies
         self.preds = self.best_est.fit(self.fullTrain).test(self.antiTest)
         
